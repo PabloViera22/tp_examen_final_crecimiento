69bff6eeeaf4faa3d79582442b7f844d033fa7c2
PabloViera22 <p4blo.viera@gmail.com>
2025-12-08 11:51:14 -0300

    prepara datos para imputar

--- scripts/03_limpieza.R
+++ scripts/03_limpieza.R
@@ -1,6 +1,7 @@
 source(here::here("config", "parametros.R"))
 source(here::here("funciones", "funcion_importa_exportar.R"))
 source(here::here("funciones", "funcion_limpieza.R"))
+source(here::here("funciones", "funcion_analisis_na.R"))
 
 tabla_a_limpiar<-importar_datos(nombre_archivo = "tabla_unida.csv",carpeta = "processed")
 
@@ -24,4 +25,29 @@ print(tabla_unida, n=10)
 exportar_data(data = tabla_unida,nombre = "tabla_con_desarrollo",carpeta = "processed")
 
 #==============================================================================#
+tabla_contar_na<-importar_datos(nombre_archivo = "tabla_con_desarrollo.csv",carpeta = "processed")
+tabla_contar_na_nombre_country<-tabla_contar_na%>% rename(country=country)
+names(tabla_contar_na)
+analizar_na(tabla = tabla_contar_na, grupo = "year")
+faltantes<-analizar_na(tabla = tabla_contar_na_nombre_country, grupo = "country")
+faltantes$por_columna %>%arrange(desc(na_pop))
+paises_a_sacar<-c("Belarus","czechia","Russian Federation", "Ukraine", "Yemen")
+
+tabla_a_imputar<-tabla_contar_na_nombre_country%>%filter(!(country %in% paises_a_sacar))
+exportar_data(data = tabla_a_imputar,nombre = "tabla_a_imputar",carpeta = "processed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
